0bed622655319be17fa7787f71c1e4d85b6f6c6c
andideve <andideve0@gmail.com>
2022-03-28 13:24:58 +0700

    move hook into src directly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export * from './hooks';
+import React from 'react';
+
+const useDebounce = (callback: () => void, [ms, deps]: [number, readonly any[] | any]) => {
+  const dependencyList = React.useMemo(() => (Array.isArray(deps) ? deps : [deps]), [deps]);
+
+  const [isWaiting, setIsWaiting] = React.useState(false);
+
+  React.useEffect(() => {
+    let callbackReturn: unknown = null;
+
+    const timeoutId = setTimeout(() => {
+      setIsWaiting(false);
+      callbackReturn = callback();
+    }, ms);
+
+    return () => {
+      clearTimeout(timeoutId);
+      setIsWaiting(true);
+      if (typeof callbackReturn === 'function') callbackReturn();
+    };
+  }, dependencyList);
+
+  return { isWaiting } as const;
+};
+
+export { useDebounce };
+export default useDebounce;
